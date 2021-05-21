fe23bfa6bd7a3a3e0f47204604119b72b12a3fbe
Zekarias Negassa <zakfena@gmail.com>
2021-05-21 12:05:04 -0500

    updated product model - added name

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,70 +1,78 @@
 import mongoose from 'mongoose'
 
-const reviewSchema = mongoose.Schema({
-    name: {type: String, required:true},
-    rating: {type: String, required:true},
-    comment: {type: String, required:true}
-}, {
-    timestamps: true
-})
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: String, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-
-const productSchema = mongoose.Schema({
-   user: {
-       type: mongoose.Schema.Types.ObjectId,
-       required: true,
-       ref: 'User'
-   },
-   image: {
-       type: String,
-       required: true
-   },
-    
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
     image: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
     },
     brand: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    
+
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    reviews: [reviewSchema],    
+    reviews: [reviewSchema],
     rating: {
-        type: Number,
-        required: true,
-        default:0
+      type: Number,
+      required: true,
+      default: 0,
     },
-    
+
     numReviews: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
-    
+
     price: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
 
     countInStock: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
-    
-}, {
-    timestamp: true
-})
+  },
+  {
+    timestamp: true,
+  }
+)
 
 const Product = mongoose.model('Product', productSchema)
 
